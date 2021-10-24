0325cdf2b83cf4f699264e45892ed7733b4ea2d1
Daniel <wallacedaniel999@gmail.com>
2021-10-23 18:54:43 -0700

    update site uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  //uri: 'http://localhost:4000',
+  uri: 'https://dw-graphql-test.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
